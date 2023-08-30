b90f4553b1d3a81f4508bd763b73bcf3d5eb4993
PiotrLarkowski <p.larkowski90@gmail.com>
2023-08-30 19:23:46 +0200

    Refactor SetValueOfSpecifiecWord class

---
+++ src/main/java/org/example/FindingWords.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class FindingWords {
+//    choseWords - słowa znalezione do krzyzowki
+
+}
--- src/main/java/org/example/SetValueOfSpecificWord.java
+++ src/main/java/org/example/SetValueOfSpecificWord.java
@@ -6,16 +6,32 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SetValueOfSpecificWord extends JPanel implements ActionListener {
 
     JButton returnButton, setEnableTextFieldButton, applyWordButton;
     JComboBox wordChooser;
-    JTextField Letter1TextField, Letter2TextField, Letter3TextField, Letter4TextField, Letter5TextField,
-            Letter6TextField, Letter7TextField, Letter8TextField, Letter9TextField, Letter10TextField,
-            Letter11TextField, Letter12TextField, Letter13TextField, ExtraTextField;
+    JTextField Letter1TextField = new JTextField(),
+            Letter2TextField = new JTextField(),
+            Letter3TextField = new JTextField(),
+            Letter4TextField = new JTextField(),
+            Letter5TextField = new JTextField(),
+            Letter6TextField = new JTextField(),
+            Letter7TextField = new JTextField(),
+            Letter8TextField = new JTextField(),
+            Letter9TextField = new JTextField(),
+            Letter10TextField = new JTextField(),
+            Letter11TextField = new JTextField(),
+            Letter12TextField = new JTextField(),
+            Letter13TextField = new JTextField(),
+            ExtraTextField;
 
     JLabel topLabel, middleLabel;
+    ArrayList<JTextField> textFieldList = new ArrayList<>(Arrays.asList(Letter1TextField, Letter2TextField,
+            Letter3TextField, Letter4TextField, Letter5TextField, Letter6TextField, Letter7TextField,
+            Letter8TextField, Letter9TextField, Letter10TextField, Letter11TextField, Letter12TextField, Letter13TextField));
 
     public SetValueOfSpecificWord() {
 
@@ -89,55 +105,42 @@ public class SetValueOfSpecificWord extends JPanel implements ActionListener {
                 });
         add(wordChooser);
 
-        Letter1TextField = new JTextField();
         Letter1TextField.setBounds(180, 150, 30, 60);
         add(Letter1TextField);
 
-        Letter2TextField = new JTextField();
         Letter2TextField.setBounds(220, 150, 30, 60);
         add(Letter2TextField);
 
-        Letter3TextField = new JTextField();
         Letter3TextField.setBounds(260, 150, 30, 60);
         add(Letter3TextField);
 
-        Letter4TextField = new JTextField();
         Letter4TextField.setBounds(300, 150, 30, 60);
         add(Letter4TextField);
 
-        Letter5TextField = new JTextField();
         Letter5TextField.setBounds(340, 150, 30, 60);
         add(Letter5TextField);
 
-        Letter6TextField = new JTextField();
         Letter6TextField.setBounds(380, 150, 30, 60);
         add(Letter6TextField);
 
-        Letter7TextField = new JTextField();
         Letter7TextField.setBounds(420, 150, 30, 60);
         add(Letter7TextField);
 
-        Letter8TextField = new JTextField();
         Letter8TextField.setBounds(460, 150, 30, 60);
         add(Letter8TextField);
 
-        Letter9TextField = new JTextField();
         Letter9TextField.setBounds(500, 150, 30, 60);
         add(Letter9TextField);
 
-        Letter10TextField = new JTextField();
         Letter10TextField.setBounds(540, 150, 30, 60);
         add(Letter10TextField);
 
-        Letter11TextField = new JTextField();
         Letter11TextField.setBounds(580, 150, 30, 60);
         add(Letter11TextField);
 
-        Letter12TextField = new JTextField();
         Letter12TextField.setBounds(620, 150, 30, 60);
         add(Letter12TextField);
 
-        Letter13TextField = new JTextField();
         Letter13TextField.setBounds(660, 150, 30, 60);
         add(Letter13TextField);
 
@@ -150,60 +153,50 @@ public class SetValueOfSpecificWord extends JPanel implements ActionListener {
         g.setColor(Color.white);
         g.fillRect(50, 50, 700, 400);
     }
-    private void setThreeTextFieldActive() {
-        Letter1TextField.setEnabled(true);
-        Letter2TextField.setEnabled(true);
-        Letter3TextField.setEnabled(true);
-    }
-    private void setFourTextFieldActive() {
-        setThreeTextFieldActive();
-        Letter4TextField.setEnabled(true);
-    }
-    private void setFiveTextFieldActive() {
-        setFourTextFieldActive();
-        Letter5TextField.setEnabled(true);
-    }
-    private void setSixTextFieldActive() {
-        setFiveTextFieldActive();
-        Letter6TextField.setEnabled(true);
-    }
-    private void setSevenTextFieldActive() {
-        setSixTextFieldActive();
-        Letter7TextField.setEnabled(true);
-    }
-    private void setThirteenTextFieldActive() {
-        setSevenTextFieldActive();
-        Letter8TextField.setEnabled(true);
-        Letter9TextField.setEnabled(true);
-        Letter10TextField.setEnabled(true);
-        Letter11TextField.setEnabled(true);
-        Letter12TextField.setEnabled(true);
-        Letter13TextField.setEnabled(true);
+    private void setTextFieldActive(int numberOfTextFieldToActive) {
+        for (int i = 0; i < numberOfTextFieldToActive; i++) {
+            textFieldList.get(i).setEnabled(true);
+        }
     }
 
     private void setAllTextFieldDisabled() {
-        Letter1TextField.setEnabled(false);
-        Letter2TextField.setEnabled(false);
-        Letter3TextField.setEnabled(false);
-        Letter4TextField.setEnabled(false);
-        Letter5TextField.setEnabled(false);
-        Letter6TextField.setEnabled(false);
-        Letter7TextField.setEnabled(false);
-        Letter8TextField.setEnabled(false);
-        Letter9TextField.setEnabled(false);
-        Letter10TextField.setEnabled(false);
-        Letter11TextField.setEnabled(false);
-        Letter12TextField.setEnabled(false);
-        Letter13TextField.setEnabled(false);
+        for (JTextField jTextField : textFieldList) {
+            jTextField.setEnabled(false);
+        }
     }
     private void setEnableTextFields() {
         setAllTextFieldDisabled();
+        int[][] numbersOfWordsWithSameLetters = {{1,4,12,13,15,21,30,32},{2,3,6,8,9,14,16,17,22,24,25,29,31,33,35,37},
+                {5,23,26,27,28},{7,10,34,36},{19,20}};
+//        for (int i = 0; i < 5; i++) {
+//            for (int j = 0; j < numbersOfWordsWithSameLetters[i].length; j++) {
+//                if(wordChooser.getSelectedItem().equals(numbersOfWordsWithSameLetters[i][j])){
+//                    setTextFieldActive(i);
+//                }
+//            }
+//        }
+        int[] numbersOfWordsWithSevenLetters = {1,4,12,13,15,21,30,32};
+        int[] numbersOfWordsWithFiveLetters = {2,3,6,8,9,14,16,17,22,24,25,29,31,33,35,37};
+        int[] numbersOfWordsWithSixLetters = {5,23,26,27,28};
+        int[] numbersOfWordsWithFourLetters = {7,10,34,36};
+        int[] numbersOfWordsWithTreeLetters = {19,20};
+        for (int numbersOfWordsWithSevenLetter : numbersOfWordsWithSevenLetters) {
+            if (wordChooser.getSelectedItem().equals(numbersOfWordsWithSevenLetter)) {
+                setTextFieldActive(7);
+            }
+        }
+        for (int i = 0; i < numbersOfWordsWithFiveLetters.length; i++) {
+            if(wordChooser.getSelectedItem().equals(numbersOfWordsWithFiveLetters[i])){
+                setTextFieldActive(5);
+                break;
+            }
+        }
         if (wordChooser.getSelectedItem().equals(1) || wordChooser.getSelectedItem().equals(4) ||
                 wordChooser.getSelectedItem().equals(12) || wordChooser.getSelectedItem().equals(13) ||
                 wordChooser.getSelectedItem().equals(15) || wordChooser.getSelectedItem().equals(18) ||
                 wordChooser.getSelectedItem().equals(21) || wordChooser.getSelectedItem().equals(30) ||
                 wordChooser.getSelectedItem().equals(32)) {
-            setSevenTextFieldActive();
+            setTextFieldActive(7);
         } else if (wordChooser.getSelectedItem().equals(2) || wordChooser.getSelectedItem().equals(3) ||
                 wordChooser.getSelectedItem().equals(6) || wordChooser.getSelectedItem().equals(8) ||
                 wordChooser.getSelectedItem().equals(9) || wordChooser.getSelectedItem().equals(14) ||
@@ -212,19 +205,30 @@ public class SetValueOfSpecificWord extends JPanel implements ActionListener {
                 wordChooser.getSelectedItem().equals(25) || wordChooser.getSelectedItem().equals(29) ||
                 wordChooser.getSelectedItem().equals(31) || wordChooser.getSelectedItem().equals(33) ||
                 wordChooser.getSelectedItem().equals(35) || wordChooser.getSelectedItem().equals(37)){
-            setFiveTextFieldActive();
+            setTextFieldActive(5);
         } else if (wordChooser.getSelectedItem().equals(5) || wordChooser.getSelectedItem().equals(23) ||
                 wordChooser.getSelectedItem().equals(26) || wordChooser.getSelectedItem().equals(27) ||
                 wordChooser.getSelectedItem().equals(28)) {
-            setSixTextFieldActive();
+            setTextFieldActive(6);
         } else if (wordChooser.getSelectedItem().equals(7) || wordChooser.getSelectedItem().equals(10) ||
                 wordChooser.getSelectedItem().equals(34) || wordChooser.getSelectedItem().equals(36)) {
-            setFourTextFieldActive();
+            setTextFieldActive(4);
         } else if (wordChooser.getSelectedItem().equals(19) || wordChooser.getSelectedItem().equals(20)) {
-            setThreeTextFieldActive();
-        } else if (wordChooser.getSelectedItem().equals(11)) {
-            setThirteenTextFieldActive();
+            setTextFieldActive(3);
         }
+        if (wordChooser.getSelectedItem().equals(11)) {
+            setTextFieldActive(13);
+        }
+    }
+    private void combineValueOfLettersIntoWord() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < textFieldList.size(); i++) {
+            if(textFieldList.get(i).isEnabled()){
+                stringBuilder.append(textFieldList.get(i).getText());
+            }
+        }
+        JOptionPane.showMessageDialog(this, "Value has been set / Wartosc zostala ustawiona");
+        MainView.choseWords.set(wordChooser.getSelectedIndex(), stringBuilder.toString());
     }
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -234,8 +238,7 @@ public class SetValueOfSpecificWord extends JPanel implements ActionListener {
         }else if(event == setEnableTextFieldButton){
             setEnableTextFields();
         }else if(event == applyWordButton){
-
+            combineValueOfLettersIntoWord();
         }
-
     }
 }
